abf53e1cc58d59fc380d2a36f996e4cac76c4027
yuki okuno(`・ω・́)ゝ <okuno@ap-com.co.jp>
2018-12-17 16:10:51 +0900

    enemy

--- invader03.py
+++ invader03.py
@@ -5,30 +5,30 @@ from pygame.locals import *
 import os
 import sys
 
-SCR_RECT = Rect(0, 0, 1200, 1000)
+SCR_RECT = Rect(0, 0, 640, 480)
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode(SCR_RECT.size)
-    pygame.display.set_caption(u"Gofer_Atack")
+    pygame.display.set_caption(u"Invader 03 エイリアン襲来")
     # サウンドのロード
     Player.shot_sound = load_sound("shot.wav")
     # スプライトグループを作成して登録
     all = pygame.sprite.RenderUpdates()
     Player.containers = all
     Shot.containers = all
+    Alien.containers = all
     # スプライトの画像を登録
-    Player.image = load_image("goAK.png")
+    Player.image = load_image("player.png")
     Shot.image = load_image("shot.png")
-    # 自機を作成
     Alien.images = split_image(load_image("alien.png"), 2)
-    Player()　#プレイヤー作成
-
+    # 自機を作成
+    Player()
+    # エイリアンを作成
     for i in range(0, 50):
         x = 20 + (i % 10) * 40
         y = 20 + (i / 10) * 40
         Alien((x,y))
-
     
     clock = pygame.time.Clock()
     while True:
@@ -47,22 +47,22 @@ def main():
 
 class Player(pygame.sprite.Sprite):
     """自機"""
-    speed = 10  # 移動速度
-    reload_time = 5  # リロード時間
+    speed = 5  # 移動速度
+    reload_time = 15  # リロード時間
     def __init__(self):
         # imageとcontainersはmain()でセット
         pygame.sprite.Sprite.__init__(self, self.containers)
         self.rect = self.image.get_rect()
-        self.rect.bottom = SCR_RECT.left  
+        self.rect.bottom = SCR_RECT.bottom  # プレイヤーが画面の一番下
         self.reload_timer = 0
     def update(self):
         # 押されているキーをチェック
         pressed_keys = pygame.key.get_pressed()
         # 押されているキーに応じてプレイヤーを移動
-        if pressed_keys[K_UP]:
-            self.rect.move_ip(0, -self.speed)
-        elif pressed_keys[K_DOWN]:
-            self.rect.move_ip(0, self.speed)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-self.speed, 0)
+        elif pressed_keys[K_RIGHT]:
+            self.rect.move_ip(self.speed, 0)
         self.rect.clamp_ip(SCR_RECT)
         # ミサイルの発射
         if pressed_keys[K_SPACE]:
@@ -76,6 +76,19 @@ class Player(pygame.sprite.Sprite):
                 Shot(self.rect.center)  # 作成すると同時にallに追加される
                 self.reload_timer = self.reload_time
 
+class Shot(pygame.sprite.Sprite):
+    """プレイヤーが発射するミサイル"""
+    speed = 9  # 移動速度
+    def __init__(self, pos):
+        # imageとcontainersはmain()でセット
+        pygame.sprite.Sprite.__init__(self, self.containers)
+        self.rect = self.image.get_rect()
+        self.rect.center = pos  # 中心座標をposに
+    def update(self):
+        self.rect.move_ip(0, -self.speed)  # 上へ移動
+        if self.rect.top < 0:  # 上端に達したら除去
+            self.kill()
+
 class Alien(pygame.sprite.Sprite):
     """エイリアン"""
     speed = 2  # 移動速度
@@ -88,8 +101,8 @@ class Alien(pygame.sprite.Sprite):
         self.image = self.images[0]
         self.rect = self.image.get_rect()
         self.rect.center = pos
-        self.top = pos[0]  # 移動できる左端
-        self.bottom = self.left + self.move_width  # 移動できる右端
+        self.left = pos[0]  # 移動できる左端
+        self.right = self.left + self.move_width  # 移動できる右端
     def update(self):
         # 横方向への移動
         self.rect.move_ip(self.speed, 0)
@@ -99,27 +112,14 @@ class Alien(pygame.sprite.Sprite):
         self.frame += 1
         self.image = self.images[self.frame/self.animcycle%2]
 
-class Shot(pygame.sprite.Sprite):
-    """プレイヤーが発射するミサイル"""
-    speed = 10  # 移動速度
-    def __init__(self, pos):
-        # imageとcontainersはmain()でセット
-        pygame.sprite.Sprite.__init__(self, self.containers)
-        self.rect = self.image.get_rect()
-        self.rect.center = pos  # 中心座標をposに
-    def update(self):
-        self.rect.move_ip(self.speed, 0)  # 上へ移動
-        if self.rect.right < 0:  # 上端に達したら除去
-            self.kill()
-
 def load_image(filename, colorkey=None):
     """画像をロードして画像と矩形を返す"""
-    filename = os.path.join("./", filename)
+    filename = os.path.join("data", filename)
     try:
         image = pygame.image.load(filename)
-    except pygame.error as message:
-        print ("Cannot load image:", filename)
-        raise SystemExit 
+    except pygame.error, message:
+        print "Cannot load image:", filename
+        raise SystemExit, message
     image = image.convert()
     if colorkey is not None:
         if colorkey is -1:
@@ -127,8 +127,23 @@ def load_image(filename, colorkey=None):
         image.set_colorkey(colorkey, RLEACCEL)
     return image
 
+def split_image(image, n):
+    """横に長いイメージを同じ大きさのn枚のイメージに分割
+    分割したイメージを格納したリストを返す"""
+    image_list = []
+    w = image.get_width()
+    h = image.get_height()
+    w1 = w / n
+    for i in range(0, w, w1):
+        surface = pygame.Surface((w1,h))
+        surface.blit(image, (0,0), (i,0,w1,h))
+        surface.set_colorkey(surface.get_at((0,0)), RLEACCEL)
+        surface.convert()
+        image_list.append(surface)
+    return image_list
+
 def load_sound(filename):
-    filename = os.path.join("./", filename)
+    filename = os.path.join("data", filename)
     return pygame.mixer.Sound(filename)
 
 if __name__ == "__main__":
